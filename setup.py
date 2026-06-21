5b09d18f42cea3ffabcd3fc421354d993b846823
24sbahmad <24sbahmad@gmail.com>
2026-06-21 00:09:10 -0400

    Feat: Add SQL setup, Kaggle API integration, and GitHub Actions workflow

---
+++ setup.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('football.db')
+pd.read_csv('results.csv').to_sql('results', conn, if_exists='replace', index=False)
+pd.read_csv('shootouts.csv').to_sql('shootouts', conn, if_exists='replace', index=False)
+print("Created")
+
+import os
+import sqlite3
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+os.environ['KAGGLE_USERNAME'] = os.getenv('KAGGLE_USERNAME', 'bilala28')
+os.environ['KAGGLE_KEY'] = os.getenv('KAGGLE_KEY', 'a51ca52ea80432ba1530efce407c52ac')
+
+api = KaggleApi()
+api.authenticate()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print("Downloading latest dataset from Kaggle...")
+api.dataset_download_files('martj42/international-football-results-from-1872-to-2017', path=BASE_DIR, unzip=True)
+
+db_path = os.path.join(BASE_DIR, 'football.db')
+conn = sqlite3.connect(db_path)
+
+pd.read_csv(os.path.join(BASE_DIR, 'results.csv')).to_sql('results', conn, if_exists='replace', index=False)
+print("Updated!")
